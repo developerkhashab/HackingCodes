32204df2cdcf0cc84ed724f15e651cd507245673
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-04-02 23:12:06 -0400

    first commit

---
+++ Mac_Changer/mac_changer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    parser=optparse.OptionParser()
+    parser.add_option('-i','--interface',dest='interface',help='interface to change its mac address')
+    parser.add_option('-m','--mac',dest='new_mac',help='new mac address')
+    return parser.parse_args()
+
+def change_mac(interface,new_mac):
+    print('[+] Changing mac address for '+interface+' to '+new_mac)
+    subprocess.call(['ifconfig',interface,'down'])
+    subprocess.call(['ifconfig',interface,'hw','ether',new_mac])
+    subprocess.call(['ifconfig',interface,'up'])
+
+def get_current_mac(interface):
+    ifconfig_result=subprocess.check_output(['ifconfig',interface]).decode('utf-8')
+    mac_address_search_result=re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w:',ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print('[+] Could not read MAC address.')
+
+options=get_arguments()
+# print(options[0].interface)
+
+current_mac=get_current_mac(options[0].interface)
+print('current MAC = '+ str(current_mac))
+
+change_mac(options[0].interface,options[0].new_mac)
+
+current_mac=get_current_mac(options[0].interface)
+if current_mac==options[0].new_mac:
+    print('[+] MAC address was successfully changed to '+ current_mac)
+else:
+    print('[+] MAC address did not get changed')
---
+++ Network_Scanner/network_scanner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import argparse
+import scapy.all as scapy
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
+    return options
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        clients_dic = {'ip': element[1].psrc, 'mac': element[1].hwsrc}
+        clients_list.append(clients_dic)
+    return clients_list
+
+
+def print_result(results_list):
+    print('IP\t\t\t AT MAC \n************************************************')
+    for client in results_list:
+        print(client["ip"] + '\t\t' + client["mac"])
+
+option=get_arguments()
+scan_results = scan(option.target)
+print_result(scan_results)
+# '10.0.2.0/24'
