807603475fbbc1e7b13240c82ba2ae3537166656
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-04-05 04:06:58 -0400

    Adding packet sniffer file

--- Packet_Sniffer/packet_sniffer.py
+++ Packet_Sniffer/packet_sniffer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 import time
 import scapy.all as scapy
+
+def sniff (interface):
+    scapy.sniff(iface=interface,store=False,prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    print(packet)
